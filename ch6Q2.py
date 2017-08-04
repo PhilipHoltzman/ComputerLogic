a371ca3afdfbf9cf921c384cb46e2913ec46d534
Philip Holtzman <phil.holtzman@gmail.com>
2017-08-04 07:04:28 -0500

    day 3

---
+++ ch6Q2.py
@@ -0,0 +1,13 @@
+# Philip Holtzman
+# Programming Logic
+# Week 4 Ch.6Assignment #2
+
+
+
+def feet2inches(feet):
+  inches = 12*feet
+  print("There is %d inches in %d feet " % (inches, feet))
+  
+feet = int(input("Please enter the number of feet: "))
+
+feet2inches(feet)
